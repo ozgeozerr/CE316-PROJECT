2a469191aea19fa7c048521895f52f3a39136613
ozgeozerr <ozerrrozge@gmail.com>
2024-05-02 20:18:57 +0300

    özge son deneme ;)

--- BerkazyHalka/obj/Debug/net8.0-windows/BerkazyHalka.AssemblyInfo.cs
+++ BerkazyHalka/obj/Debug/net8.0-windows/BerkazyHalka.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BerkazyHalka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642dcae36e912a98eaadbb80450e035ee681d238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13080a57899c27b3e119852f170a95acc31d1528")]
 [assembly: System.Reflection.AssemblyProductAttribute("BerkazyHalka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BerkazyHalka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
