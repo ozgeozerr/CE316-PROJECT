084e26b6663a13a1a289a1993033ede7b9ceb788
ahmetalpsamur <ahmetalpsamur@gmail.com>
2024-05-09 16:52:25 +0300

    error fixed

--- BerkazyHalka/obj/Debug/net8.0-windows/BerkazyHalka.AssemblyInfo.cs
+++ BerkazyHalka/obj/Debug/net8.0-windows/BerkazyHalka.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BerkazyHalka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a66618d0d8c6cdb7c49271954681561cb15dbda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4dcdd7553ffbada543bfeafbf5a5ba253591617")]
 [assembly: System.Reflection.AssemblyProductAttribute("BerkazyHalka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BerkazyHalka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
