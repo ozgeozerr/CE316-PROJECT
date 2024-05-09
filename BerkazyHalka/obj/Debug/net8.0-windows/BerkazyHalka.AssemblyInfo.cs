f2b6da87263b9a3ab700bd06440013b9cee49bf8
ahmetalpsamur <ahmetalpsamur@gmail.com>
2024-05-09 20:05:16 +0300

    new data base relation (added configuration file)

--- BerkazyHalka/obj/Debug/net8.0-windows/BerkazyHalka.AssemblyInfo.cs
+++ BerkazyHalka/obj/Debug/net8.0-windows/BerkazyHalka.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BerkazyHalka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4dcdd7553ffbada543bfeafbf5a5ba253591617")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5847e4264e1f7a96c1965fd4338772eab9b80d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BerkazyHalka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BerkazyHalka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
